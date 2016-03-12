605e1101ab518583f2dd6ff7b4a339f99aa6d78d
Evgeni Puzankov <evgeni.puzankov@gmail.com>
2016-03-13 01:58:42 +0300

    Cutscene, Menu, Some shite.

---
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class ButtonBehaviour : MonoBehaviour, ISelectHandler, IDeselectHandler
+{
+
+    public Animator Anim;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        Anim.SetBool("Selected", true);
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        Anim.SetBool("Selected", false);
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    public Button CommenceBtn;
+    public Button AbortBtn;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetAxisRaw("Vertical") > 0) {
+            CommenceBtn.Select();
+            
+        }
+        if(Input.GetAxisRaw("Vertical") < 0)
+        {
+            AbortBtn.Select();
+        }
+	}
+
+    public void Commence()
+    {
+        SceneManager.LoadScene("");
+    }
+
+    public void Abort()
+    {
+        Application.Quit();
+    }
+}
